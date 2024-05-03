536c44d48764e40bd8fb5905f5e5f601f0226619
Tiago-Amaral <tiagolima323@gmail.com>
2024-05-03 19:13:00 -0300

    add api/camisas

--- src/server.js
+++ src/server.js
@@ -11,17 +11,39 @@ app.use(express.json());
 app.use(cors());
 
 // Rota para lidar com a requisição da API do Mercado Livre
-app.get('/api', async (req, res) => {
+app.get('/api/celulares', async (req, res) => {
   try {
     // Faz a solicitação para a API do Mercado Livre para buscar celulares
-    const response = await axios.get('http://api.mercadolibre.com/sites/MLB/search?q=celular');
+    const response1 = await axios.get('http://api.mercadolibre.com/sites/MLB/search?q=celular');
+    const celulares = response1.data;
     
     // Retorna os dados da resposta da API do Mercado Livre para o cliente
-    res.json(response.data);
+    res.json(celulares);
   } catch (error) {
     console.error('Erro ao buscar celulares:', error);
     // Retorna um status de erro 500 e uma mensagem de erro para o cliente
     res.status(500).send('Erro ao buscar celulares na API do Mercado Livre.');
+  }
+});
+
+
+
+//segunda api de camisas
+
+
+
+app.get('/api/camisas', async (req, res) => {
+  try {
+    // Faz a solicitação para a API do Mercado Livre para buscar celulares
+    const response2 = await axios.get('http://api.mercadolibre.com/sites/MLB/search?q=camisas');
+    const camisas = response2.data;
+    
+    // Retorna os dados da resposta da API do Mercado Livre para o cliente
+    res.json(camisas);
+  } catch (error) {
+    console.error('Erro ao buscar camisas:', error);
+    // Retorna um status de erro 500 e uma mensagem de erro para o cliente
+    res.status(500).send('Erro ao buscar camisas na API do Mercado Livre.');
   }
 });
 
